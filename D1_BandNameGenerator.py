c5f85c249623b2d6e4dc48ddad54f4883fbe17e8
Joao Custodio <68780183+MADMAVERICK22@users.noreply.github.com>
2022-01-10 22:40:42 -0300

    Add files via upload

---
+++ D1_BandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to your band name generator!!")
+
+cname = input("Where were you born?\n")
+
+yname = input("What's your name?\n")
+
+print("your band name could be " + '{} {}'.format(cname, yname))
