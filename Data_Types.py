7a16dc70497fbf13ee1624009561f56bca2d5fc0
Joao Custodio <68780183+MADMAVERICK22@users.noreply.github.com>
2022-01-11 19:14:28 -0300

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,11 @@
+#a variable to store the value
+two_digit_number = input("Type any two digit number:\n")
+
+#finding the variable's type
+# -> print(type(two_digit_number))
+
+#making a sum with them after converted to int
+f1 = int(two_digit_number[0])
+f2 = int(two_digit_number[1])
+
+print(f1 + f2)
